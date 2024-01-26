70d891013e9ad4ff8aa5b71fe570790b857740ff
Vitaliy Fomin <fomin_v.v@mail.ru>
2024-01-26 21:10:29 +0500

    lesson01 create main.lava

---
+++ lesson01/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal genericAnimal = new Animal();
+        FlyingAnimal bird = new FlyingAnimal();
+        SwimmingAnimal fish = new SwimmingAnimal();
+
+        // Проверяем полиморфизм
+        genericAnimal.toGo();
+        genericAnimal.fly();
+        genericAnimal.swim();
+
+        bird.toGo();
+        bird.fly();
+        bird.swim(); 
+
+        fish.toGo();
+        fish.fly(); 
+        fish.swim();
+    }
+}
