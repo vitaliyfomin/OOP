1a9f185b0e4103d6a65669ba0382027f1c0c6d51
Vitaliy Fomin <fomin_v.v@mail.ru>
2024-01-27 11:52:36 +0500

    branch lesson01_v02 Penguin.java

---
+++ lesson01/src/Penguin.java
@@ -0,0 +1,11 @@
+public class Penguin extends Animal {
+    @Override
+    public void swim() {
+        System.out.println("Пингвин плавает");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Пингвин бегает");
+    }
+}
