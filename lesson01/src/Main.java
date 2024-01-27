30bbfb3aafaef95dd9294c2306bd96e9ae3db0fe
Vitaliy Fomin <fomin_v.v@mail.ru>
2024-01-27 11:57:18 +0500

    branch lesson01_v02 Main.java

--- lesson01/src/Main.java
+++ lesson01/src/Main.java
@@ -3,18 +3,58 @@ public class Main {
         Animal genericAnimal = new Animal();
         FlyingAnimal bird = new FlyingAnimal();
         SwimmingAnimal fish = new SwimmingAnimal();
+        RunningAnimal cheetah = new RunningAnimal();
+        DivingAnimal penguin = new DivingAnimal();
+        Duck duck = new Duck();
+        Penguin penguinInstance = new Penguin();
 
-        // Проверяем полиморфизм
         genericAnimal.toGo();
         genericAnimal.fly();
         genericAnimal.swim();
+        genericAnimal.run();
+        genericAnimal.quack();
+        genericAnimal.dive();
 
         bird.toGo();
         bird.fly();
-        bird.swim(); 
+        bird.swim();
+        bird.run();
+        bird.quack();
+        bird.dive();
 
         fish.toGo();
-        fish.fly(); 
+        fish.fly();
         fish.swim();
+        fish.run();
+        fish.quack();
+        fish.dive();
+
+        cheetah.toGo();
+        cheetah.fly();
+        cheetah.swim();
+        cheetah.run();
+        cheetah.quack();
+        cheetah.dive();
+
+        penguin.toGo();
+        penguin.fly();
+        penguin.swim();
+        penguin.run();
+        penguin.quack();
+        penguin.dive();
+
+        duck.toGo();
+        duck.fly();
+        duck.swim();
+        duck.run();
+        duck.quack();
+        duck.dive();
+
+        penguinInstance.toGo();
+        penguinInstance.fly();
+        penguinInstance.swim();
+        penguinInstance.run();
+        penguinInstance.quack();
+        penguinInstance.dive();
     }
 }
