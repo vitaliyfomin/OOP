efb3aab6dbc37f3f1edf42887ee2e5b0aba4e403
Vitaliy Fomin <fomin_v.v@mail.ru>
2024-02-17 21:23:43 +0500

    add getReal() and getImag() in class MathComplex

--- lesson07/src/calculator/entities/MathComplex.java
+++ lesson07/src/calculator/entities/MathComplex.java
@@ -1,13 +1,8 @@
 package calculator.entities;
 
 public class MathComplex {
-    private final double real;
-    private final double imag;
-
-    public MathComplex() {
-        this.real = 0;
-        this.imag = 0;
-    }
+    private double real;
+    private double imag;
 
     public MathComplex(double real, double imag) {
         this.real = real;
@@ -22,8 +17,8 @@ public class MathComplex {
         return imag;
     }
 
-    public MathComplex add(MathComplex other) {
-        return new MathComplex(this.real + other.real, this.imag + other.imag);
+    @Override
+    public String toString() {
+        return real + " + " + imag + "i";
     }
-
 }
